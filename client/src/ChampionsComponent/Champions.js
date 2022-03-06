e1f922ffc71008752df747eb2bd35b7a98c1c0f2
gez1985 <gez.egan@gmail.com>
2022-03-06 20:42:15 +0000

    small champs order change

--- client/src/ChampionsComponent/Champions.js
+++ client/src/ChampionsComponent/Champions.js
@@ -2,11 +2,21 @@ import RenderChampions from "./RenderChampions";
 
 export default function Champions() {
   const iplChampions = [
-    { year: 2021, champion: "Matthew Joss", runnerUp: "Gerard Egan", woodenSpoon: "Tom Kent" },
     { year: 2022, champion: "?", runnerUp: "?", woodenSpoon: "Tom Kent" },
+    {
+      year: 2021,
+      champion: "Matthew Joss",
+      runnerUp: "Gerard Egan",
+      woodenSpoon: "Tom Kent",
+    },
   ];
   const wcChampions = [
-    { year: 2021, champion: "Can Ogan", runnerUp: "Kimberley Kings", woodenSpoon: "Tom Kent" },
+    {
+      year: 2021,
+      champion: "Can Ogan",
+      runnerUp: "Kimberley Kings",
+      woodenSpoon: "Tom Kent",
+    },
   ];
   return (
     <div className="landing-champions-container">
