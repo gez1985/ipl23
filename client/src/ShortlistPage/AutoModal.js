b27647707d51219305431a0c3f9a82d19daf7e14
gez1985 <gez.egan@gmail.com>
2023-03-15 23:03:05 +0000

    add initial player in sort

--- client/src/ShortlistPage/AutoModal.js
+++ client/src/ShortlistPage/AutoModal.js
@@ -1,19 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ManagerContext, PlayersContext } from "../Store";
 import ModalTemplate from "../ModalTemplate";
+import Search from "../utils/search";
 
 const AutoModal = ({ close, title, player }) => {
-  const [manager] = useContext(ManagerContext);
+  const [manager, setManager] = useContext(ManagerContext);
   const [players] = useContext(PlayersContext);
+  const [loading, setLoading] = useState(false);
   console.log({ manager });
+
+  const handleAddInitialPlayerClick = async () => {
+    console.log(`handle add inital player clicked`);
+    setLoading(true);
+    try {
+      const managerCopy = JSON.parse(JSON.stringify(manager));
+      managerCopy.shortlist.push(player.id);
+      await Search.putManager(managerCopy);
+      setManager(managerCopy);
+    } catch (err) {
+      console.log(err.message);
+    }
+    setLoading(false);
+    close()
+  };
 
   const getEmptyShortlist = () => {
     return (
       <div>
         <p>Your shortlist is empty</p>
         <p>Add {player.name} to your shortlist?</p>
-        <button>Yes</button>
-        <button>No</button>
+        <button onClick={handleAddInitialPlayerClick}>Yes</button>
+        <button onClick={close}>No</button>
       </div>
     );
   };
